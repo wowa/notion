e38c0c5461229a4e1e3e4fe42999d50fb3a73aa9
Wojciech Worwa <wojciech.worwa@gmail.com>
2023-02-07 09:43:15 +0100

    Use UUIDv4 for IDs instead of String

--- src/ids.rs
+++ src/ids.rs
@@ -1,8 +1,9 @@
 use std::fmt::Display;
 use std::fmt::Error;
+use uuid::Uuid;
 
 pub trait Identifier: Display {
-    fn value(&self) -> &str;
+    fn value(&self) -> String;
 }
 /// Meant to be a helpful trait allowing anything that can be
 /// identified by the type specified in `ById`.
@@ -15,7 +16,7 @@ where
     T: Identifier,
 {
     fn as_id(&self) -> &T {
-        &self
+        self
     }
 }
 
@@ -27,16 +28,15 @@ where
         self
     }
 }
-
-macro_rules! identifer {
+macro_rules! uuid_identifer {
     ($name:ident) => {
         #[derive(serde::Serialize, serde::Deserialize, Debug, Eq, PartialEq, Hash, Clone)]
         #[serde(transparent)]
-        pub struct $name(String);
+        pub struct $name(Uuid);
 
         impl Identifier for $name {
-            fn value(&self) -> &str {
-                &self.0
+            fn value(&self) -> String {
+                self.0.to_string()
             }
         }
 
@@ -50,17 +50,48 @@ macro_rules! identifer {
             type Err = Error;
 
             fn from_str(s: &str) -> Result<Self, Self::Err> {
-                Ok($name(s.to_string()))
+                Ok($name(Uuid::parse_str(s).unwrap()))
             }
         }
     };
 }
 
-identifer!(DatabaseId);
-identifer!(PageId);
-identifer!(BlockId);
-identifer!(UserId);
-identifer!(PropertyId);
+macro_rules! string_identifer {
+    ($name:ident) => {
+        #[derive(serde::Serialize, serde::Deserialize, Debug, Eq, PartialEq, Hash, Clone)]
+        #[serde(transparent)]
+        pub struct $name(String);
+
+        impl Identifier for $name {
+            fn value(&self) -> String {
+                self.0.clone()
+            }
+        }
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                self.0.fmt(f)
+            }
+        }
+
+        impl std::str::FromStr for $name {
+            type Err = Error;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                Ok($name(String::from_str(s).unwrap()))
+            }
+        }
+    };
+}
+
+// According to Notion API Reference id's are UUIDv4 (https://developers.notion.com/reference/intro#json-conventions)
+// can be represented with or without dashes.
+// Using uuid crate.
+uuid_identifer!(DatabaseId);
+uuid_identifer!(PageId);
+uuid_identifer!(BlockId);
+string_identifer!(UserId);
+string_identifer!(PropertyId);
 
 impl From<PageId> for BlockId {
     fn from(page_id: PageId) -> Self {
